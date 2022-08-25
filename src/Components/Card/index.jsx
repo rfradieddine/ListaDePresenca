8c6ecddcd5b2d068e60f2be4619c91aec8c7b6ad
Rafael <rafaelradieddine@gmail.com>
2022-08-24 23:36:22 -0300

    att project

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./styless.css";
+
+export function Card(props) {
+  return (
+    <div className="card">
+      <strong>{props.name}</strong>
+      <small>{props.time}</small>
+    </div>
+  );
+}
--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,12 +1,16 @@
-import './styles.css';
+import "./styles.css";
+
+import { Card } from "../../Components/Card";
 
 export function Home() {
   return (
-  <div className='container'>
-    <h1>Lista de presença</h1>
-    <input type="text" placeholder="Digite o nome..."/>
-    <button type="button">Adicionar</button>
-  </div>
-  )
-}
+    <div className="container">
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
 
+      <Card name="Rafael" time="10:10:21" />
+      <Card name="Igor" time="15:12:21" />
+    </div>
+  );
+}
