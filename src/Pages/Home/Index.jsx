d44ca9284ce6d5cebf5fb60e36dff6ea5b25a0d3
Rafael <rafaelradieddine@gmail.com>
2022-08-24 21:51:58 -0300

    iniciando projeto react

---
+++ src/Pages/Home/Index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function Home() {
+  return (
+  <div className='container'>
+    <h1>Lista de presença</h1>
+    <input type="text" placeholder="Digite o nome..."/>
+    <button type="button">Adicionar</button>
+  </div>
+  )
+}
+
